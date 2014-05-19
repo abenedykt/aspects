c7c3c69941ef893971aff8a626acf41eba65cb16
abenedykt <arkadiusz@benedykt.net>
2014-05-19 23:04:03 +0200

    added example service

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -1,12 +1,38 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace WebUI.Controllers
 {
     public class HomeController : Controller
     {
+	    private readonly IExternalService _service;
+
+	    public HomeController(IExternalService service)
+	    {
+		    _service = service;
+	    }
+
 	    public ActionResult Index()
-        {
-            return View();
+	    {
+		    var data = _service.GetSomeData();
+            return View(data);
         }
+	}
+
+	public interface IExternalService
+	{
+		IEnumerable<ISomeObject> GetSomeData();
+	}
+
+	public interface ISomeObject
+	{
+		string Caption { get; set; }
+		string Description { get; set; }
+	}
+
+	public class SomeObject : ISomeObject
+	{
+		public string Caption { get; set; }
+		public string Description { get; set; }
 	}
 }
